dfb6b825a7cfe754b034a3bc1e953f0ca2cef2c9
Anthony Larson <anthony.r.larson@gmail.com>
2018-01-22 17:49:11 -0600

    01/22/2018 version ejs

--- nodeJSProjects/app.js
+++ nodeJSProjects/app.js
@@ -9,12 +9,20 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 app.get('/', function (req, res) {
     res.render('index');
 });
 
 app.get('/person/:id', function (req, res) {
     res.render('person', { ID: req.params.id });
+});
+
+app.post('/person', urlencodedParser, function (req, res) {
+    res.send('Thanks!');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
 });
 
 app.listen(port);
